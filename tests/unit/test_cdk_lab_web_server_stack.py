59f4db03c0cc94449bc8747325e09f76acfc697c
Jane Doe <jane@example.org>
2024-12-10 00:11:05 -0600

    added changes to be webserver

--- tests/unit/test_cdk_lab_web_server_stack.py
+++ tests/unit/test_cdk_lab_web_server_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from WebserversDev_web_server.WebserversDev_web_server_stack import CdkLabWebServerStack
+from WebserversDev_web_server.WebserversDev_web_server_stack import WebserversDevWebServerStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cdk_lab_web_server/cdk_lab_web_server_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = CdkLabWebServerStack(app, "cdk-lab-web-server")
+    stack = WebserversDevWebServerStack(app, "WebserversDev-web-server")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
